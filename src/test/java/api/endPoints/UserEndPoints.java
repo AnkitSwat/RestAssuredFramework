cc9c9b95634bb0c5985008eb1b404be6c081ab5c
AnkitSwat <toankitsaraswat@gmail.com>
2023-04-20 23:37:44 +0530

    Rest Assured framework development Part1

---
+++ src/test/java/api/endPoints/UserEndPoints.java
@@ -0,0 +1,62 @@
+package api.endPoints;
+
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import api.payLoad.User;
+import  io.restassured.http.ContentType;
+
+import io.restassured.http.ContentType;
+
+//Created to perform Create, Update, Read, Delete to the user API
+public class UserEndPoints {
+
+		public static Response createUser(User payload)
+		{
+		Response response=given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(payload)
+			.when()
+				.post(Routes.post_url);
+			
+		return response;
+		}
+	
+		public static Response readUser(String userName)
+		{
+		Response response=given()
+					.pathParam("username", "userName")
+					
+			.when()
+				.get(Routes.get_url);
+			
+		return response;
+		}
+		
+		public static Response updateUser(String userName,User payload)
+		{
+		Response response=given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.pathParam("username", userName)
+				.body(payload)
+				
+			.when()
+				.put(Routes.put_url);
+			
+		return response;
+		}
+		
+		public static Response deleteUser(String userName)
+		{
+		Response response=given()
+					.pathParam("username", "userName")
+					
+			.when()
+				.delete(Routes.delete_url);
+			
+		return response;
+		}
+		
+}
---
+++ src/test/java/api/test/UserTest.java
@@ -0,0 +1,43 @@
+package api.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
+
+import api.endPoints.UserEndPoints;
+import api.payLoad.User;
+import io.restassured.response.Response;
+
+public class UserTest {
+
+	Faker faker;
+	User userPayload;
+	
+	@BeforeClass
+	public void setupData()
+	{
+		faker=new Faker();
+		userPayload=new User();
+		
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setFirstName(faker.name().firstName());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setLastName(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5,10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		
+	}
+	
+	@Test(priority=1)
+	public void testPostUser()
+	{
+		Response response=UserEndPoints.createUser(userPayload);
+		response.then().log().all();
+		
+		Assert.assertEquals(response.getStatusCode(),200);
+		
+	}
+}
